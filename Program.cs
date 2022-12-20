29b641e4b09b866ce90711d349c2ac8325b4a774
Marton Nemeskeri <nemeskeri.marton@gmail.com>
2022-12-20 21:48:19 +0200

    registered context as a service

--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
+using Bakery.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddDbContext<BakeryContext>();
 
 var app = builder.Build();
 
